bfb8d8db0023adfaa75081a418fe7f1e065f4a7f
taomee517 <taomee517@qq.com>
2019-05-10 19:38:04 +0800

    加入list,dict

--- basic/Dict_Demo/Dictionary.py
+++ basic/Dict_Demo/Dictionary.py
@@ -4,3 +4,7 @@ dictTemp = {"nickName": "taomee", "sex": "male"}
 print(dictTemp)
 print(dictTemp.keys())
 print(dictTemp.values())
+
+# dictRev = {"age": "30"}
+# dictTemp.copy(dictRev)
+# print(dictTemp)
