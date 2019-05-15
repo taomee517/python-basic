c2deb63310030e793a33f2673d6e5a0e6c103e84
taomee517 <taomee517@qq.com>
2019-05-15 08:59:25 +0800

    python class import

--- basic/class/Car.py
+++ basic/class/Car.py
@@ -1,6 +1,12 @@
-class Car:
-   def __init__(self):
-        self = self
 
-benz = Car()
-print(benz)
+class Car(object):
+    def __init__(self, brand):
+        self.brand = brand
+
+
+# python的属性可以动态的添加，和java不一样
+benz = Car("Benz")
+benz.plateNumber = "渝BKM435"
+benz.color = "white"
+# __dict__魔法方法可以获取对象的所有属性
+print(benz.__dict__)
