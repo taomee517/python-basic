10c5cdb1119165b8e63de4f6db639e63074b3b42
taomee517 <taomee517@qq.com>
2019-05-15 18:38:21 +0800

    touch module os and sys

---
+++ basic/os_sys/sys_demo.py
@@ -0,0 +1,16 @@
+import os, sys
+
+print(dir(sys))
+print(sys.path)
+print(sys.argv)
+print(sys.getdefaultencoding())
+print(sys.getfilesystemencoding())
+print(sys.platform)
+print(sys.modules)
+# print(sys.exit(0))
+
+
+print(os.environ)
+print(os.name)
+print(os.sep)
+print(os.linesep)
