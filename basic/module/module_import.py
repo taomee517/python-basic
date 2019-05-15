7387c6d3bc7afdbdc06bab6f1c78e7257b329af0
taomee517 <taomee517@qq.com>
2019-05-15 13:28:52 +0800

    digital number format

--- basic/module/module_import.py
+++ basic/module/module_import.py
@@ -3,5 +3,18 @@
 # 2.右键点击自己的工作空间文件夹，找到Mark Directory as 选择Source Root
 
 from module1 import self_intro
+import time
+import random
 
 self_intro("Nick")
+
+# 数字的格式化输出，参考文档：https://www.runoob.com/python/att-string-format.html
+print("当前UTC时间：{:.0f}".format(time.time()))
+
+num = 76
+print("{0} 转化为二进制的值是：{1:b}".format(num, num))
+print("{0} 转化为16进制的值是：{1:x}".format(num, num))
+
+
+x = random.randint(0, 100)
+print("随机生成一个0~100的整数：{}".format(x))
