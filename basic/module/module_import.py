4e95e15ff4c684259720f2519a9ca2d3610d27d3
taomee517 <taomee517@qq.com>
2019-05-15 13:13:10 +0800

    simple module commit

--- basic/module/module_import.py
+++ basic/module/module_import.py
@@ -1,3 +1,7 @@
+# 解决IDE不能识别自己写的module:
+# 1.打开File--> Setting—> 打开 Console下的Python Console，把选项（Add source roots to PYTHONPAT）点击勾选上
+# 2.右键点击自己的工作空间文件夹，找到Mark Directory as 选择Source Root
+
 from module1 import self_intro
 
 self_intro("Nick")
